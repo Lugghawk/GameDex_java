3d56569fe4953d2966502362c7ddd7f4daed324d
Lugghawk <acidictadpole@gmail.com>
2015-06-20 22:12:24 -0400

    Added some test of UI

--- src/main/java/com/gamedex/controller/GameDexController.java
+++ src/main/java/com/gamedex/controller/GameDexController.java
@@ -1,7 +1,18 @@
 package com.gamedex.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
 /**
  * Created by Dave on 6/20/2015.
  */
 public class GameDexController {
+
+    public Label helloLabel;
+
+    @FXML
+    public void handleButton(ActionEvent event){
+        helloLabel.setText("Yay!");
+    }
 }
