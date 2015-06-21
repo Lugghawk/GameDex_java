ea8305ed97714e84e12fc000460896766880e0e0
Lugghawk <acidictadpole@gmail.com>
2015-06-20 22:36:21 -0400

    Added initialize functionality to the controller

--- src/main/java/com/gamedex/controller/GameDexController.java
+++ src/main/java/com/gamedex/controller/GameDexController.java
@@ -1,18 +1,36 @@
 package com.gamedex.controller;
 
+import com.gamedex.persist.dao.GameDao;
+import com.gamedex.persist.model.Game;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.stereotype.Component;
 
-/**
- * Created by Dave on 6/20/2015.
- */
+import java.util.List;
+
+@Component
 public class GameDexController {
 
-    public Label helloLabel;
+    public ListView<Game> gameListView;
+
 
     @FXML
+    protected void initialize(){
+        ApplicationContext context =
+                new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        GameDao gameDao = (GameDao) context.getBean("gameDao");
+        List<Game> gameList = gameDao.getAllGames();
+        ObservableList<Game> gameObservableList = FXCollections.observableList(gameList);
+        gameListView.setItems(gameObservableList);
+    }
+    @FXML
     public void handleButton(ActionEvent event){
-        helloLabel.setText("Yay!");
+
     }
 }
--- src/main/java/com/gamedex/main/GameDexMain.java
+++ src/main/java/com/gamedex/main/GameDexMain.java
@@ -1,7 +1,5 @@
 package com.gamedex.main;
 
-import com.gamedex.persist.dao.GameDao;
-import com.gamedex.persist.model.Game;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -18,11 +16,6 @@ public class GameDexMain extends Application{
         ApplicationContext context =
                 new ClassPathXmlApplicationContext("applicationContext.xml");
 
-        GameDao gameDao = (GameDao) context.getBean("gameDao");
-
-        Game game = new Game();
-        game.setName("thingy");
-        gameDao.persist(game);
         launch(args);
     }
 
